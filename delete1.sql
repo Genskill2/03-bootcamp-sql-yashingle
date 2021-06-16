3deb9f8aae91e607f036bdd3a7d437e75a8faaa6
yashingle <63144823+yashingle@users.noreply.github.com>
2021-06-16 21:05:46 +0530

    Update delete1.sql

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-DELETE FROM subjects WHERE name='History';
-DELETE FROM books_subjects WHERE subject=9;
+DELETE FROM subjects WHERE subbjects.name='History';
+DELETE FROM books_subjects WHERE books_subjects.subject=9;
