969ac57430aca452db44783f8a981057c6196967
yashingle <63144823+yashingle@users.noreply.github.com>
2021-06-16 21:00:08 +0530

    Delete query3.sql

--- query3.sql
+++
@@ -1,7 +0,0 @@
-select b.title
-from books as b
-inner join books_subjects as bs
-on b.id = bs.book
-inner join subjects as s
-on bs.subject = s.id
-where s.name = 'Technology' or s.name = 'Politics';
